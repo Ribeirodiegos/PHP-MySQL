7cb45937593d96bc9ce23580d8b01ab6ccf9a877
Ribeirodiegos <santos.diegoribeiro@gmail.com>
2022-11-07 21:23:14 -0300

    PHP+SQL
    Curso de PHP+SQL, construindo um banco para  um sistema web.

---
+++ modulo01/detalhes.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/style.css">
+  <title>Detalhes do jogo</title>
+</head>
+<body>
+<?php
+    require_once "includes/banco.php";
+    require_once "includes/funcoes.php";
+  ?>
+  <div id="corpo">
+    <?php
+      include_once "topo.php";
+
+      $c = $_GET['cod'] ?? 0;
+      $busca = $banco->query("select * from jogos where cod='$c'");
+    ?>
+
+    <h1>Detalhes do jogo</h1>
+    <table class='detalhes'>
+      <?php
+        if (!$busca) {
+          echo "<tr><td>Busca falhou!";
+        } else {
+          if ($busca->num_rows == 1) {
+            $reg = $busca->fetch_object();
+            $t = thumb($reg->capa);
+            echo "<tr><td rowspan='3'><img src='$t' class='full'>";
+            echo "<td><h2>$reg->nome</h2>";
+            echo "Nota: ".number_format($reg->nota,1). "/10";
+            echo "<tr><td>$reg->descricao";
+            echo "<tr><td>Adm";
+          } else {
+            echo "<tr><td>Nenhum Registro encontrado.";
+          }
+        }
+      ?>
+    </table>
+    <a href="index.php"><img src="icones/icoback.png" alt=""></a>
+  </div>
+  <?php include_once "rodape.php"; ?>
+
+</body>
+</html>
---
+++ modulo01/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/style.css">
+  <title>Listagem de jogos</title>
+  
+</head>
+<body>
+  <?php
+    require_once "includes/banco.php";
+    require_once "includes/funcoes.php";
+    $ordem = $_GET['o'] ?? "n";
+
+  ?>
+
+  <div id="corpo">
+  <?php include_once "topo.php"; ?>
+
+    <h1>Escolha seu jogo</h1>
+    <form action="index.html" method="get" id="busca">
+      Ordenar:
+      <a href="index.php?o=n">Nome</a> |
+      <a href="index.php?o=p">Produtora</a> |
+      <a href="index.php?o=n1">Nota Alta</a> |
+      <a href="index.php?o=n2">Nota Baixa</a> |
+      Buscar: <input type="text" name="c" size="10" maxlength="40"/>
+      <input type="submit" value="Ok"/>
+    </form>
+    <table class="listagem">
+        <?php
+        //FAZENDO A BUSCA NO BANCO DE DADOS E ARMAZENDO EM UMA VARIAVEL.
+          $q = "select j.cod, j.nome, g.genero, p.produtora, j.capa from jogos j join generos g on j.genero = g.cod join produtoras p on j.produtora = p.cod ";
+
+          switch ($ordem) {
+            case "p":
+              $q .= "ORDER BY p.produtora";
+              break;
+
+            case "n1":
+              $q .= "ORDER BY j.nota DESC";
+              break;
+            
+            case "n2":
+              $q .= "ORDER BY j.nota ASC";
+              break;
+
+            default:
+              $q .= "ORDER BY j.nome";
+              break;
+          }
+
+          $busca = $banco->query($q);
+
+          if(!$busca) {//VERIFICANDO SE A BUSCA OCORREU
+            echo "<tr><td>Infelizmente a busca deu errado";
+          } else {//VERIFICANDO SE O NUMERO DE LINHAS PESQUISADAS FOI 0
+            if ($busca->num_rows == 0) {
+              echo "Nenhum registro foi encontrado.";
+            } else {//LOOPING MOSTRANDO OS REGISTROS PESQUISADOS UM A UM NA TABELA, ENQUANTO FOR POSSIVEL.
+              while ($reg=$busca->fetch_object()) {
+                $t = thumb($reg->capa);
+                echo "<tr><td><img src='$t' class='mini'/>";
+                echo "<td><a href='detalhes.php?cod=$reg->cod'>$reg->nome</a>";
+                echo " [$reg->genero]";
+                echo "<br>$reg->produtora";
+                echo "<td>Adm";
+            }
+          }
+        }
+        ?>
+    </table>
+  </div>
+  <?php include_once "rodape.php"; ?>
+</body>
+</html>
