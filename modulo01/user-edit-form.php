1b1288925966307513bfec28f96b7e6fd0743506
Ribeirodiegos <santos.diegoribeiro@gmail.com>
2022-11-13 22:15:27 -0300

    Finalização
    Finalizando o sistema adicionando a edição e validação das alterações.

--- modulo01/user-edit-form.php
+++ modulo01/user-edit-form.php
@@ -1,12 +1,20 @@
+<?php
+  $q = "SELECT usuario, nome, senha, tipo FROM usuarios WHERE usuario='" . $_SESSION['user'] . "'";
+
+  $busca = $banco->query($q);
+  $reg = $busca->fetch_object();
+?>
+
+
 <h1>Alteração de dados</h1>
 <form action="user-edit.php" method="post">
   <table>
     <tr><td>Usuario
-        <td><input type="text" name="usuario" id="usuario" maxlength="10" size="10">
+        <td><input type="text" name="usuario" id="usuario" maxlength="10" size="10" readonly value="<?php echo $reg->usuario?>">
     <tr><td>Nome
-        <td><input type="text" name="nome" id="nome" maxlength="30" size="30">
+        <td><input type="text" name="nome" id="nome" maxlength="30" size="30" value="<?php echo $reg->nome?>">
     <tr><td>Tipo
-        <td><input type="text" name="tipo" id="tipo" readonly>
+        <td><input type="text" name="tipo" id="tipo" readonly value="<?php echo $reg->tipo?>">
     <tr><td>Senha
         <td><input type="password" name="senha1" id="senha1" maxlength="10" size="10">
     <tr><td>Confirme a senha
--- modulo01/user-edit.php
+++ modulo01/user-edit.php
@@ -23,10 +23,38 @@
         if (!isset($_POST['usuario'])) {
           include "user-edit-form.php";
         } else {
-          echo msg_sucesso("Dados foram recebidos");
+          $usuario = $_POST['usuario'] ?? null;
+          $nome = $_POST['nome'] ?? null;
+          $tipo = $_POST['tipo'] ?? null;
+          $senha1 = $_POST['senha1'] ?? null;
+          $senha2 = $_POST['senha2'] ?? null;
+
+          $q = "UPDATE usuarios SET usuario = '$usuario', nome = '$nome'";
+
+          if (empty($senha1) || is_null($senha1)) {
+            echo msg_aviso("Senha antiga foi mantida.");
+          } else {
+            if ($senha1 === $senha2) {
+              $senha = gerarHash($senha1);
+              $q .= ", senha = '$senha'";
+            } else {
+              echo msg_erro("senhas nao conferem. A senha anterior sera mantida.");
+            }
+          }
+          
+          $q .= " where usuario = '" . $_SESSION['user'] . "'";
+
+          if ($banco->query($q)) {
+            echo msg_sucesso("Usuario alterado com sucesso!");
+            logout();
+            echo msg_aviso("Por segurança,  efetue o <a href='user-login.php'>login</a> novamente.");
+          } else {
+            echo msg_erro("Nao foi possivel alterar os dados.");
+          }
         }
       }
     ?>
+    <?php echo voltar() ?>
 </div>
 <?php require_once "rodape.php"; ?>
 
